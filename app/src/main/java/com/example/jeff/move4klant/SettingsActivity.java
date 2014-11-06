7f64cd8b56941b63bab63feb975fbdb204457416
Sander Wubs <sanderwubs@gmail.cmo>
2014-11-06 15:06:19 +0100

    1e basis gelegd voor de klant app  - Home scherm  - Login  - Register  - Settings
    Library:
     - APIFunctions  (hier kunnen functies gemaakt worden om API's aan te roepen)
     - ibeacon object klasse
     - JSONParser die tussen de APIFunctions en de API functioneert als parser van data
     - PrefUtils kan instellingen opslaan en ophalen
    
    Groot aantal aan resources files waaronder plaatjes, overgangsanimaties en layouts

---
+++ app/src/main/java/com/example/jeff/move4klant/SettingsActivity.java
@@ -0,0 +1,73 @@
+package com.example.jeff.move4klant;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import library.PrefUtils;
+
+/**
+ * Created by Sander on 6-11-2014.
+ */
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        addPreferencesFromResource(R.xml.settings);
+
+
+
+        Preference myPref = (Preference) findPreference("prefDisconnectAccount");
+        myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+               disconnectAccount();
+                return true;
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    @Override
+    public void onBackPressed(){
+        finish();
+        this.overridePendingTransition(R.anim.right_slide_in, R.anim.right_slide_out);
+    }
+
+
+
+
+
+    public void disconnectAccount() {
+        PrefUtils.saveToPrefs(SettingsActivity.this, getString(R.string.PREFS_LOGIN_USERNAME_KEY), "");
+        PrefUtils.saveToPrefs(SettingsActivity.this, getString(R.string.PREFS_LOGIN_PASSWORD_KEY), "");
+        PrefUtils.getFromPrefs(SettingsActivity.this, getString(R.string.PREFS_AUTO_LOGIN_KEY), "false");
+
+        Toast.makeText(getApplicationContext(),"Account ontkoppeld", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+}
---
+++ app/src/main/java/library/APIFunctions.java
@@ -0,0 +1,148 @@
+package library;
+
+/**
+ * Created by Sander on 8-10-2014.
+ */
+import android.content.Context;
+
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class APIFunctions {
+    private static APIFunctions _instance = null;
+
+    private JSONParser jsonParser;
+
+    /***************
+     *EXPLANATION API URLs
+     ***************
+     * Add here the URL of your PHP API, it should look like this:
+     * private static String your_API_URL = "http://example.com/your_API/";
+     **/
+    private static String loginURL = "http://supermonsieurbalzaqsmashbros.comxa.com/login_api/";
+    private static String registerURL = "http://supermonsieurbalzaqsmashbros.comxa.com/login_api/";
+    private static String forpassURL = "http://supermonsieurbalzaqsmashbros.comxa.com/login_api/";
+    private static String chgpassURL = "http://supermonsieurbalzaqsmashbros.comxa.com/login_api/";
+
+
+    //constructor
+    private APIFunctions(){
+        jsonParser = new JSONParser();
+    }
+
+    //create instance
+    private synchronized static void createInstance () {
+        if (_instance == null) _instance = new APIFunctions ();
+    }
+
+    //get instance
+    public static APIFunctions getInstance () {
+        if (_instance == null) createInstance ();
+        return _instance;
+    }
+
+    /***************
+     *EXPLANATION
+     ***************
+     * To call your API and retrieve the JSON object,
+     * create a new function like this:
+     *
+     *         public JSONObject yourFunction(String yourParameter <<as many as you want>>){
+     *
+     *              List params = new ArrayList();
+     *              params.add(new BasicNameValuePair("yourKey", yourParameter));
+     *
+     * // now, the jsonParser will retrieve the JSON object from your API URL, and will send the parameters with the POST request
+     *
+     *              JSONObject json = jsonParser.getJSONFromUrl(your_API_URL, params);
+     *              return json;
+     *          }
+     *
+     *
+     * To use this in your own class,
+     * just call the function you created in this document like this:
+     *
+     *      APIFunctions someName = APIFunctions.getInstance();
+     *      JSONObject json = someName.yourFunction(yourParameters);
+     *
+     * Now you can use the information the API has returned
+     **/
+
+
+
+    /**
+     * Function to Login
+     **/
+    public JSONObject loginUser(String email, String password){
+        // Building Parameters
+        List params = new ArrayList();
+        params.add(new BasicNameValuePair("tag", "login"));
+        params.add(new BasicNameValuePair("email", email));
+        params.add(new BasicNameValuePair("password", password));
+        JSONObject json = jsonParser.getJSONFromUrl(loginURL, params);
+        return json;
+    }
+    /**
+     * Function to change password
+     **/
+    public JSONObject chgPass(String newpas, String email){
+        List params = new ArrayList();
+        params.add(new BasicNameValuePair("tag", "chgpass"));
+        params.add(new BasicNameValuePair("newpas", newpas));
+        params.add(new BasicNameValuePair("email", email));
+        JSONObject json = jsonParser.getJSONFromUrl(chgpassURL, params);
+        return json;
+    }
+    /**
+     * Function to reset the password
+     **/
+    public JSONObject forPass(String forgotpassword){
+        List params = new ArrayList();
+        params.add(new BasicNameValuePair("tag", "forpass"));
+        params.add(new BasicNameValuePair("forgotpassword", forgotpassword));
+        JSONObject json = jsonParser.getJSONFromUrl(forpassURL, params);
+        return json;
+    }
+    /**
+     * Function to  Register
+     **/
+    public JSONObject registerUser(String fname, String lname, String email, String uname, String password){
+        // Building Parameters
+        List params = new ArrayList();
+        params.add(new BasicNameValuePair("tag", "register"));
+        params.add(new BasicNameValuePair("fname", fname));
+        params.add(new BasicNameValuePair("lname", lname));
+        params.add(new BasicNameValuePair("email", email));
+        params.add(new BasicNameValuePair("uname", uname));
+        params.add(new BasicNameValuePair("password", password));
+        JSONObject json = jsonParser.getJSONFromUrl(registerURL,params);
+        return json;
+    }
+    /**
+     * Function to logout user
+     * Resets the temporary data stored in SQLite Database
+     * */
+    public boolean logoutUser(Context context){
+        DatabaseHandler db = new DatabaseHandler(context);
+        db.resetTables();
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
